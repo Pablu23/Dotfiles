31ee269cabf4687c87f1c1e8d6e3374f8844023d
pablu <me@pablu.de>
2026-04-29 17:57:20 +0200

    nvim

--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -1,6 +1,6 @@
 return {
   "saghen/blink.cmp",
-  dependecies = { "rafamadriz/friendly-snippets" },
+  dependencies = { "rafamadriz/friendly-snippets" },
   version = "1.*",
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
--- dot_config/nvim/lua/vim-options.lua
+++ dot_config/nvim/lua/vim-options.lua
@@ -1,11 +1,23 @@
 vim.g.mapleader = " "
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set expandtab")
-vim.cmd("set clipboard+=unnamedplus")
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.clipboard:append("unnamedplus")
+
 vim.cmd("highlight Normal guibg=none")
 vim.cmd("highlight NonText guibg=none")
 vim.cmd("highlight Normal ctermbg=none")
 vim.cmd("highlight NonText ctermbg=none")
+
 vim.o.winborder = 'rounded'
+
+vim.opt.undofile = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.cursorline = true
+vim.opt.signcolumn = "yes"
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
