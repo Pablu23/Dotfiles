5984d43664ccdc10ae41ab5cf0b3e9a0f0dd6ce7
Zam Kokott <zam.kokott@bwi.de>
2026-06-05 10:51:12 +0000

    update nvim

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -3,9 +3,9 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
     require('lualine').setup({
-      options = {
-        theme = 'catppuccin'
-      },
+      -- options = {
+      --   theme = 'catppuccin'
+      -- },
       sections = {
       lualine_x = {
         function()
